3eeb3b3c987bca9e0742b7549204522d8de3061d
James <jamesbrendamour7@yahoo.com>
2024-04-15 21:19:01 -0600

    Refactor chat component UI and update chat URL
    - Refactored the chat component UI styling for better readability
    - Updated the URL for the embedded Streamlit chat in the iframe
    Refactor chat component UI and update chat URL
    
    - Refactored the chat component UI styling for better readability
    - Updated the URL for the embedded Streamlit chat in the iframe

--- src/components/streamlit-chat/index.tsx
+++ src/components/streamlit-chat/index.tsx
@@ -1,9 +1,4 @@
-import React, { useState } from 'react';
-
-interface Message {
-  text: string;
-  sender: 'user' | 'bot';
-}
+import React, { useState } from 'react'
 
 const StreamlitApp: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,13 +7,16 @@ const StreamlitApp: React.FC = () => {
 
   return (
     <div className="fixed bottom-5 right-5 z-50">
-      <button onClick={toggleChat} className="px-5 py-2.5 bg-blue-800 text-white rounded-lg cursor-pointer focus:outline-none">
+      <button
+        onClick={toggleChat}
+        className="px-5 py-2.5 bg-blue-800 text-white rounded-lg cursor-pointer focus:outline-none"
+      >
         {isOpen ? 'Close Chat' : 'Chat'}
       </button>
       {isOpen && (
-        <div className="absolute bottom-12 right-0 w-[800px] h-[600px] bg-gray-900 border border-gray-300 shadow-2xl rounded-lg overflow-hidden">
+        <div className="absolute bottom-12 right-0 w-[900px] h-[800px] bg-gray-900 border border-gray-300 shadow-2xl rounded-lg overflow-hidden">
           <iframe
-            src="https://pdfchat-eustkxjfhdghra4qgvs2yr.streamlit.app/?embed=true"
+            src="https://portfolio-chat-jdb.streamlit.app/?embed=true"
             title="Streamlit Chat"
             className="w-full h-full border-none"
           />
