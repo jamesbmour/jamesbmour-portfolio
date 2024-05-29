1d9d770b70fc15b11f016aba3d4fd0a945924ccd
James <jamesbrendamour3@gmail.com>
2024-05-29 13:03:39 -0600

    Refactor code in previews.tsx for clarity
    This commit updates the code in previews.tsx. Specifically, SanitizedBlog has been imported from 'interfaces/sanitized-config.tsx', a semi-colon has been added after a blog declaration for neater code, and there is also slight changes in spacing. The export statement also has been modified to add a newline at the end of the file for better code quality.

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,16 +1,17 @@
 import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox';
 import { PaletteTree } from './palette';
 import BlogCard from '../components/blog-card';
+import { SanitizedBlog } from '../interfaces/sanitized-config.tsx';
 
 interface ComponentPreviewsProps {
-  blog: SanitizedBlog
+  blog: SanitizedBlog;
 }
 
 const ComponentPreviews = ({ blog }: ComponentPreviewsProps) => {
   return (
     <Previews palette={<PaletteTree />}>
       <ComponentPreview path="/BlogCard">
-        <BlogCard blog={blog} loading/>
+        <BlogCard blog={blog} loading />
       </ComponentPreview>
     </Previews>
   );
